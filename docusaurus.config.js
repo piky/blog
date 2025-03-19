9920df0db9dac692c37d796ca2231d576554bd34
Wijai Thongsom <wijai.thongsom@gmail.com>
2025-03-19 15:37:40 +0700

    Correct gramma in tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Playground, My Memory',
-  tagline: 'Knowledge belong to mankind, experience belong to individual.',
+  tagline: 'Knowledge belongs to mankind; experience belongs to a person.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
