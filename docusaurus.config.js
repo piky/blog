6133d4e371bcb7c8473c3ff022e102406b349d07
Wijai Thongsom <wijai.thongsom@gmail.com>
2025-07-19 15:34:38 +0700

    Display last update info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/piky/blog/blob/main',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
